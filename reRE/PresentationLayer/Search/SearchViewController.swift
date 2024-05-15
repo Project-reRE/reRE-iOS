189f4bcbde597d3d8d8c469819b2354b551aa554
kch1285 <kch1285@naver.com>
2024-05-15 17:55:16 +0900

    feat: SearchVC UI 작업

---
+++ reRE/PresentationLayer/Search/SearchViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SearchViewController.swift
+//  reRE
+//
+//  Created by chihoooon on 2024/04/10.
+//
+
+import UIKit
+import SnapKit
+
+final class SearchViewController: BaseViewController {
+    var coordinator: SearchBaseCoordinator?
+    
+    private lazy var searchMovieView = SearchCategoryView(category: .movie)
+    private lazy var searchBookView = SearchCategoryView(category: .book)
+    private lazy var searchMusicView = SearchCategoryView(category: .music)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = ColorSet.gray(.gray10).color
+    }
+    
+    override func addViews() {
+        view.addSubviews([searchMovieView, searchBookView, searchMusicView])
+    }
+    
+    override func makeConstraints() {
+        searchMovieView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(moderateScale(number: 60) + getSafeAreaTop())
+            $0.leading.trailing.equalToSuperview().inset(moderateScale(number: 16))
+            $0.height.equalTo(moderateScale(number: 69))
+        }
+        
+        searchBookView.snp.makeConstraints {
+            $0.top.equalTo(searchMovieView.snp.bottom).offset(moderateScale(number: 8))
+            $0.leading.trailing.equalTo(searchMovieView)
+            $0.height.equalTo(moderateScale(number: 69))
+        }
+        
+        searchMusicView.snp.makeConstraints {
+            $0.top.equalTo(searchBookView.snp.bottom).offset(moderateScale(number: 8))
+            $0.leading.trailing.equalTo(searchMovieView)
+            $0.height.equalTo(moderateScale(number: 69))
+        }
+    }
+    
+    override func setupIfNeeded() {
+        
+    }
+}
